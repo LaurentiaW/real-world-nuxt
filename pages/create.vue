40038d05a49ccd5001bb7171568a26697ff182f6
LaurentiaW <laurentiawehmeyer@gmail.com>
2019-09-19 12:22:08 +0200

    added SEO tags with vue meta in nuxt -default fall back for each page in layout and a title and description on each page that is different

--- pages/create.vue
+++ pages/create.vue
@@ -3,3 +3,21 @@
     <h1>Create an Event</h1>
   </div>
 </template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Here you can list an event taking place in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
