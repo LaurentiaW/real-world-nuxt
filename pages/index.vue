899e80f450680dc8e3e97a4520831f100a02f676
LaurentiaW <laurentiawehmeyer@gmail.com>
2019-09-20 15:25:22 +0200

    add vuex to project and state to the eventslisting page as well as the individual events pages

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,10 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import EventCard from '@/components/EventCard.vue'
+// now making use of VUEX and not calling from EventService but from the store
+// import EventService from '@/services/EventService.js'
 
 export default {
   head() {
@@ -39,18 +42,34 @@ export default {
   //     })
   // }
   // using Async Await
-  async asyncData({ $axios, error }) {
+  // async asyncData({ $axios, error }) {
+
+  // remove $axios when using the EventService
+  // async asyncData({ error }) {
+
+  // using vuex thus not asyncData but fetch  and add the store to the arguments
+  async fetch({ store, error }) {
     try {
-      const { data } = await $axios.get('http://localhost:3000/events')
-      return {
-        events: data
-      }
+      // const { data } = await $axios.get('http://localhost:3000/events')
+      // adding the event service thus this syntax gets updated to incl this rather than making the call it self
+      //   const { data } = await EventService.getEvents()
+      //   return {
+      //     events: data
+      //   }
+      // }
+
+      // for vuex remove the returned values and call store dispatch events, fetchEvents
+      await store.dispatch('events/fetchEvents')
     } catch (e) {
       error({
         statusCode: 503,
         message: 'Unable to show events at this time. Please try again.'
       })
     }
-  }
+  },
+  // for Vuex add computed property use mapState to map the Events property
+  computed: mapState({
+    events: (state) => state.events.events
+  })
 }
 </script>
